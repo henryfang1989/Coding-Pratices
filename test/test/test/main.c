0a7e291e7d07a9dab32fa9e1cd7e6cd42e6e94cc
henry fang <fanghuangwei@gmail.com>
2015-10-07 21:22:47 -0700

    just for test

---
+++ test/test/test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test
+//
+//  Created by Huangwei Fang on 10/7/15.
+//  Copyright © 2015 henryfang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
